21482b122b0bdbbc1b0e51ef45d955275a00e67e
minigun20050 <144325105+minigun20050@users.noreply.github.com>
2023-10-09 11:44:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let intNumber = 5;
+let floatNumber = 78.4846;
+let boolean = true;
+let string = "Ala ma kota";
+let undef;
+let nullVar = null;
+let object = {
+
+};
+let array = [];
+
+console.log(2 + 8);
+console.log(2 + "8");
+console.log(floatNumber + 4834.1347853497);
+console.log(floatNumber - (4834.1347853497 + intNumber));
+console.log(string + floatNumber - (boolean + intNumber));
