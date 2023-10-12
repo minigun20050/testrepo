64b287422dc8b1a4f01ef2e6979d072467ce0b1b
minigun20050 <144325105+minigun20050@users.noreply.github.com>
2023-10-12 11:19:55 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-let intNumber = 5;
-let floatNumber = 78.4846;
-let boolean = true;
-let string = "Ala ma kota";
-let undef;
-let nullVar = null;
-let object = {
+/* let liczba = 15;
+let tekst = "15";
 
-};
-let array = [];
+if (liczba == tekst)
+    console.log("wartości są równe");
 
-console.log(2 + 8);
-console.log(2 + "8");
-console.log(floatNumber + 4834.1347853497);
-console.log(floatNumber - (4834.1347853497 + intNumber));
-console.log(string + floatNumber - (boolean + intNumber));
+if(liczba === parseInt(tekst)) 
+    console.log("Wartości są równe i typy są zgodne");
+else 
+    console.log("Wartości nie są równe i typy nie są zgodne");*/
+
+let x = prompt("Wpisz perwszą liczbę");
+let y = prompt("Wpisz drugą liczbę");
+
+x = parseInt(x);
+y = parseInt(y);
+
+let sum = x + y;
+
+if(sum % 3 == 0) {
+    console.log(x * y);
+} else if(sum % 3 == 1) {
+    console.log(y / x);
+} else {
+    console.log(x ** (x -y));
+}
