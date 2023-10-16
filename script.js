d1eec3da1cf60edcd7ad1cbfcd19595a4b931271
minigun20050 <144325105+minigun20050@users.noreply.github.com>
2023-10-16 13:10:21 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,7 @@
-/* let liczba = 15;
-let tekst = "15";
+let y = parseInt(prompt());
 
-if (liczba == tekst)
-    console.log("wartości są równe");
-
-if(liczba === parseInt(tekst)) 
-    console.log("Wartości są równe i typy są zgodne");
-else 
-    console.log("Wartości nie są równe i typy nie są zgodne");*/
-
-let x = prompt("Wpisz perwszą liczbę");
-let y = prompt("Wpisz drugą liczbę");
-
-x = parseInt(x);
-y = parseInt(y);
-
-let sum = x + y;
-
-if(sum % 3 == 0) {
-    console.log(x * y);
-} else if(sum % 3 == 1) {
-    console.log(y / x);
-} else {
-    console.log(x ** (x -y));
-}
+console.log((y == 1) ? ("Wybrałeś wartość 1") : (
+    (y == 2) ? ("Wybrałeś wartość 2") : (
+        (y == 3) ? ("Wybrałeś wartość 3") : ("101")))
+    )
+    
