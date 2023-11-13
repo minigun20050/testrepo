74da9144cda680f1f33ad5d03e6e36d8dc235b35
minigun20050 <144325105+minigun20050@users.noreply.github.com>
2023-11-13 13:09:07 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-let a
-a = prompt ('a = ')
-function factorial(number) {
-    number = parseInt(number);
-    if(!isNaN(number)) {
-        if(number === 0) {
-            return 1;
-        }
-        else {
-            return number * factorial(number - 1);
-        }
+// function factorial(number) {
+//     number = parseInt(number);
+//     if(!isNaN(number) && number >= 0) {
+//         if(number === 0) {
+//             return 1;
+//         }
+//         else {
+//             return number * factorial(number - 1);
+//         }
+//     }
+// }
+
+// factorial(prompt("Wprowadz wartosc liczbowa"));
+
+// function fibonacci(n) {
+//     n = parseInt(n);
+//     if(!isNaN(n)) {
+//         if(n < 2)
+//         return n;
+//     else
+//         return fibonacci(n - 2) + fibonacci(n - 1);
+//     }
+// }
+
+let n = parseInt(prompt("Wprowadz dodatnia wartosc liczbowa"));
+if(n <= 0)
+    console.log("Miala byc dodatnia... bandyto!");
+else {
+    for (let i = 0; i < n; i++) {
+        console.log(fibonacci(i));
     }
 }
 
-factorial(a);
+fibonacci(5);
