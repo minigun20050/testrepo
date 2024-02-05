aa6db6af900b897dacbf2cc18b1841e8fbd19cb3
minigun20050 <144325105+minigun20050@users.noreply.github.com>
2024-02-05 13:19:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,104 @@
-class Figure {
-    Perimeter() { }
-    Field() { }
-}
-class Triangle extends Figure {
-    #a;
-    #b;
-    #c;
-    constructor(a, b, c) {
-        super();
-        this.#a = a;
-        this.#b = b;
-        this.#c = c;
+class Animal {
+    constructor(eat, sound) {
+        this.eat = eat;
+        this.sound = sound;
     }
-    Perimeter() {
-        return this.#a + this.#b + this.#c;
+    makeSound() {
+        return this.sound;
     }
-    Field() {
-        let p = (this.#a + this.#b + this.#c) / 2;
-        return Math.sqrt(p * (p - this.#a) * (p - this.#b) * (p - this.#c));
+    takeEat() {
+        return this.eat;
     }
 }
-const triangle = new Triangle(2, 4, 5); 
+
+class Dog extends Animal {
+    constructor(eat, sound, color) {
+        super(eat, sound);
+        this.color = color;
+    }
+    makeSound(times = 1) {
+        let i = 1;
+        let sound = '';
+        do {
+            sound += this.sound;
+            i++;
+        } while(i < times);
+        return sound;
+    }
+}
+
+const d1 = new Dog('🥩', 'Woof!', 'czarny');
+d1.makeSound(2);
+
+class Fish extends Animal {
+    constructor(eat, sound, color, swim) {
+        super(eat, sound);
+        this.color = color;
+        this.swim = swim;
+    }
+    makeSound(times = 1) {
+        let i = 1;
+        let sound = '';
+        do {
+            sound += this.sound;
+            i++;
+        } while(i < times);
+        return sound;
+    }
+    doSwim(times = 1) {
+        let i = 1;
+        let swim = '';
+        do {
+            swim += this.swim;
+            i++;
+        } while(i < times);
+        return swim;
+    }
+}
+
+const d2 = new Fish('🥖', 'Bul ', 'biała', 'swimming ');
+d2.makeSound(2);
+
+class Worker {
+    constructor(name, surname, total) {
+        this.name = name;
+        this.surname = surname;
+        this.total = total;
+    }
+    totalYear() {
+        let total = 0;
+        let i = 0;
+        total += this.total;
+        i = 12*total;
+        return i;
+    }
+}
+
+const d3 = new Worker('Steve', 'Klubnika', 20000);
+
+class Manager extends Worker {
+    constructor(name, surname, total, tips) {
+        super(name, surname, total);
+        this.tips = tips;
+    }
+    totalYear() {
+        let total = 0;
+        let i = 0;
+        let tips = 0;
+        total += this.total;
+        tips += this.tips;
+        i = total*tips*12*12;
+        return i;
+    }
+    totalMonth() {
+        let total = 0;
+        let i = 0;
+        let tips = 0;
+        total += this.total;
+        tips += this.tips;
+        i = total*tips+total;
+        return i;
+    }
+}
+
+const d4 = new Manager('Mike', 'Vazovski', 20000, 0.1);
