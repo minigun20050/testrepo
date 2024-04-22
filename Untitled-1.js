59f7de49072bb7e6275a74bd3d97e749f5ed4604
minigun20050 <144325105+minigun20050@users.noreply.github.com>
2024-04-22 12:29:38 +0200

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,60 @@
+
+// 1 zrob
+function concat(text) {
+    return text.slice(0, 1) + text.slice(6, 7);
+}
+concat("okulary")
+
+// 2
+function spacja(text) {
+    text = "okulary";
+}
+console.log(text.slice(0, 1) + " " + text.slice(1, 2) + " " + text.slice(2, 3) + " " + text.slice(3, 4) + " " + text.slice(4, 5))
+
+// 3
+function odw(text) {
+    text = "1234";
+}
+console.log(text + text.replace(1234, 4321))
+
+// 4
+function pla(text) {
+    text = "okulary";
+}
+console.log(text.pla())
+
+// 5
+function czas(text) {
+    text = "okulary";
+}
+console.log(text.czas())
+
+// 6
+function alf(text) {
+    text = "okulary";
+}
+console.log(text.alf())
+
+// 7
+function spell(text) {
+    text = "okulary";
+}
+console.log(text.spell())
+
+// 8
+function palin(text) {
+    text = "okulary";
+}
+console.log(text.palin())
+
+// 9
+function count(text) {
+    text = "okulary";
+}
+console.log(text.count())
+
+// 10
+function vovcount(text) {
+    text = "okulary";
+}
+console.log(text.vovcount())
