62dc659592bb91957aff4ef762e5e8522660aca5
minigun20050 <144325105+minigun20050@users.noreply.github.com>
2024-04-25 11:30:03 +0200

    Add files via upload

---
+++ ćw/Untitled-1.js
@@ -0,0 +1,60 @@
+
+// 1 zrob
+function concat(text) {
+    return text[0] + text[text.length-1];
+}
+concat("okulary")
+
+// 2
+function spacja(text) {
+    return text[0] + " " + text[1] + " " + text[2] + " " + text[3] + " " + text[4] + " " + text[5];
+}
+spacja("blabla")
+
+// 3
+function odw(text) {
+    return text + text.reverse
+}
+odw("1234")
+
+// 4
+function pla(text) {
+    return
+}
+pla("")
+
+// 5
+function czas(text) {
+    return
+}
+czas("")
+
+// 6
+function alf(text) {
+    return
+}
+alf("")
+
+// 7
+function spell(text) {
+    return
+}
+spell("")
+
+// 8
+function palin(text) {
+    return
+}
+palin("")
+
+// 9
+function count(text) {
+    return
+}
+count("")
+
+// 10
+function vovcount(text) {
+    return
+}
+vovcount("")
