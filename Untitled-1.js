0a053c6057262c4e8d472cb056f95e6fd7239449
minigun20050 <144325105+minigun20050@users.noreply.github.com>
2024-04-29 13:10:51 +0200

    Add files via upload

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,60 +1,96 @@
 
-// 1 zrob
-function concat(text) {
-    return text.slice(0, 1) + text.slice(6, 7);
-}
-concat("okulary")
+// // 1 zrob
+// function concat(text) {
+//     return text[0] + text[text.length-1];
+// }
+// concat("okulary")
 
-// 2
-function spacja(text) {
-    text = "okulary";
-}
-console.log(text.slice(0, 1) + " " + text.slice(1, 2) + " " + text.slice(2, 3) + " " + text.slice(3, 4) + " " + text.slice(4, 5))
+// // 2
+// function spacja(text) {
+//     return text[0] + " " + text[1] + " " + text[2] + " " + text[3] + " " + text[4] + " " + text[5];
+// }
+// spacja("blabla")
 
-// 3
-function odw(text) {
-    text = "1234";
-}
-console.log(text + text.replace(1234, 4321))
+// // 3
+// function odw(text) {
+//     return text + text.reverse
+// }
+// odw("1234")
 
-// 4
-function pla(text) {
-    text = "okulary";
-}
-console.log(text.pla())
+// // 4
+// function pla(text) {
+//     return
+// }
+// pla("")
 
-// 5
-function czas(text) {
-    text = "okulary";
-}
-console.log(text.czas())
+// // 5
+// function czas(text) {
+//     return
+// }
+// czas("")
 
-// 6
-function alf(text) {
-    text = "okulary";
-}
-console.log(text.alf())
+// // 6
+// function alf(text) {
+//     return
+// }
+// alf("")
 
-// 7
-function spell(text) {
-    text = "okulary";
-}
-console.log(text.spell())
+// // 7
+// function spell(text) {
+//     return
+// }
+// spell("")
 
-// 8
-function palin(text) {
-    text = "okulary";
-}
-console.log(text.palin())
+// // 8
+// function palin(text) {
+//     return
+// }
+// palin("")
 
-// 9
-function count(text) {
-    text = "okulary";
-}
-console.log(text.count())
+// // 9
+// function count(text) {
+//     return
+// }
+// count("")
 
-// 10
-function vovcount(text) {
-    text = "okulary";
+// // 10
+// function vovcount(text) {
+//     return
+// }
+// vovcount("")
+
+// // 11
+// function x(text) {
+//     return text[0];
+// }
+// x("text")
+
+// //12
+// function y(text) {
+//     return text[0] + text[text.length-1];
+// }
+// y("texty")
+
+// //13
+// function count(text) {
+//     return Array.from(text.matchAll('Krowa')).length;
+// }
+// count('Krowa 1 23')
+
+
+
+function fun(x) {
+    return x*(Math.random());
 }
-console.log(text.vovcount())
+fun(5)
+// without round
+function nofun(x) {
+    return Math.round(x*(Math.random()));
+}
+nofun(10)
+// with round
+function realfun(x, y) {
+    return Math.ceil(Math.random() * (y - 1+x));
+}
+realfun(1, 5)
+// from x to y with ceil
