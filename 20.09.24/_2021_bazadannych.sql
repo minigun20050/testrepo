8c85c9bc0ce09215037e3ee18a1b121317eec11f
Fl5r <144325105+minigun20050@users.noreply.github.com>
2024-09-20 13:11:48 +0200

    Add files via upload

--- 20.09.24/_2021_bazadannych.sql
+++ 20.09.24/_2021_bazadannych.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Хост: 127.0.0.1
--- Время создания: Сен 13 2024 г., 13:16
--- Версия сервера: 10.4.32-MariaDB
--- Версия PHP: 8.2.12
+-- Host: 127.0.0.1
+-- Generation Time: Sep 20, 2024 at 01:10 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,26 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- База данных: `_2021_bazadannych`
+-- Database: `_2021_bazadannych`
 --
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `stanowiska`
+-- Table structure for table `adresy`
+--
+
+CREATE TABLE `adresy` (
+  `id` int(11) NOT NULL,
+  `id.id` int(11) DEFAULT NULL,
+  `third` varchar(20) DEFAULT NULL,
+  `fourth` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stanowiska`
 --
 
 CREATE TABLE `stanowiska` (
@@ -33,7 +46,7 @@ CREATE TABLE `stanowiska` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Дамп данных таблицы `stanowiska`
+-- Dumping data for table `stanowiska`
 --
 
 INSERT INTO `stanowiska` (`id`, `nazwa`) VALUES
@@ -42,54 +55,77 @@ INSERT INTO `stanowiska` (`id`, `nazwa`) VALUES
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `tabela`
+-- Table structure for table `tabela`
 --
 
 CREATE TABLE `tabela` (
   `id` int(11) NOT NULL,
   `imie` varchar(50) DEFAULT NULL,
   `nazwisko` varchar(100) DEFAULT NULL,
-  `stanowisko` tinyint(4) DEFAULT NULL
+  `stanowisko` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Дамп данных таблицы `tabela`
+-- Dumping data for table `tabela`
 --
 
 INSERT INTO `tabela` (`id`, `imie`, `nazwisko`, `stanowisko`) VALUES
 (1, 'Ala', 'Makota', 1);
 
 --
--- Индексы сохранённых таблиц
+-- Indexes for dumped tables
 --
 
 --
--- Индексы таблицы `stanowiska`
+-- Indexes for table `adresy`
+--
+ALTER TABLE `adresy`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `stanowiska`
 --
 ALTER TABLE `stanowiska`
   ADD PRIMARY KEY (`id`);
 
 --
--- Индексы таблицы `tabela`
+-- Indexes for table `tabela`
 --
 ALTER TABLE `tabela`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `stanowisko` (`stanowisko`);
 
 --
--- AUTO_INCREMENT для сохранённых таблиц
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT для таблицы `stanowiska`
+-- AUTO_INCREMENT for table `adresy`
+--
+ALTER TABLE `adresy`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `stanowiska`
 --
 ALTER TABLE `stanowiska`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT для таблицы `tabela`
+-- AUTO_INCREMENT for table `tabela`
 --
 ALTER TABLE `tabela`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tabela`
+--
+ALTER TABLE `tabela`
+  ADD CONSTRAINT `tabela_ibfk_1` FOREIGN KEY (`stanowisko`) REFERENCES `stanowiska` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
