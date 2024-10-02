6b46b83eda7a671f8d8e9e3063f90e27df9be8b1
Fl5r <144325105+minigun20050@users.noreply.github.com>
2024-10-02 13:19:22 +0200

    Add files via upload

---
+++ 02.10.24/script.js
@@ -0,0 +1,150 @@
+window.addEventListener('DOMContentLoaded', () => {
+    let dropImage = document.querySelector('#dropImage1');
+    let dragImage = document.querySelector('#dragImage');
+    dragImage.addEventListener('dragstart', (e) => {
+        e.dataTransfer.setData("text", e.target.id);
+    });
+    dropImage.addEventListener('dragover', (e) => {
+        e.preventDefault();
+    });
+    dropImage.addEventListener('drop', (e) => {
+        e.preventDefault();
+        let data = e.dataTransfer.getData("text");
+        e.target.appendChild(document.querySelector('#' + data));
+    });
+});
+window.addEventListener('DOMContentLoaded', () => {
+    let dropImage = document.querySelector('#dropImage2');
+    let dragImage = document.querySelector('#dragImage');
+    dragImage.addEventListener('dragstart', (e) => {
+        e.dataTransfer.setData("text", e.target.id);
+    });
+    dropImage.addEventListener('dragover', (e) => {
+        e.preventDefault();
+    });
+    dropImage.addEventListener('drop', (e) => {
+        e.preventDefault();
+        let data = e.dataTransfer.getData("text");
+        e.target.appendChild(document.querySelector('#' + data));
+    });
+});
+window.addEventListener('DOMContentLoaded', () => {
+    let dropImage = document.querySelector('#dropImage3');
+    let dragImage = document.querySelector('#dragImage');
+    dragImage.addEventListener('dragstart', (e) => {
+        e.dataTransfer.setData("text", e.target.id);
+    });
+    dropImage.addEventListener('dragover', (e) => {
+        e.preventDefault();
+    });
+    dropImage.addEventListener('drop', (e) => {
+        e.preventDefault();
+        let data = e.dataTransfer.getData("text");
+        e.target.appendChild(document.querySelector('#' + data));
+    });
+});
+window.addEventListener('DOMContentLoaded', () => {
+    let dropImage = document.querySelector('#dropImage4');
+    let dragImage = document.querySelector('#dragImage');
+    dragImage.addEventListener('dragstart', (e) => {
+        e.dataTransfer.setData("text", e.target.id);
+    });
+    dropImage.addEventListener('dragover', (e) => {
+        e.preventDefault();
+    });
+    dropImage.addEventListener('drop', (e) => {
+        e.preventDefault();
+        let data = e.dataTransfer.getData("text");
+        e.target.appendChild(document.querySelector('#' + data));
+    });
+});
+window.addEventListener('DOMContentLoaded', () => {
+    let dropImage = document.querySelector('#dropImage5');
+    let dragImage = document.querySelector('#dragImage');
+    dragImage.addEventListener('dragstart', (e) => {
+        e.dataTransfer.setData("text", e.target.id);
+    });
+    dropImage.addEventListener('dragover', (e) => {
+        e.preventDefault();
+    });
+    dropImage.addEventListener('drop', (e) => {
+        e.preventDefault();
+        let data = e.dataTransfer.getData("text");
+        e.target.appendChild(document.querySelector('#' + data));
+    });
+});
+window.addEventListener('DOMContentLoaded', () => {
+    let dropImage = document.querySelector('#dropImage6');
+    let dragImage = document.querySelector('#dragImage');
+    dragImage.addEventListener('dragstart', (e) => {
+        e.dataTransfer.setData("text", e.target.id);
+    });
+    dropImage.addEventListener('dragover', (e) => {
+        e.preventDefault();
+    });
+    dropImage.addEventListener('drop', (e) => {
+        e.preventDefault();
+        let data = e.dataTransfer.getData("text");
+        e.target.appendChild(document.querySelector('#' + data));
+    });
+});
+window.addEventListener('DOMContentLoaded', () => {
+    let dropImage = document.querySelector('#dropImage7');
+    let dragImage = document.querySelector('#dragImage');
+    dragImage.addEventListener('dragstart', (e) => {
+        e.dataTransfer.setData("text", e.target.id);
+    });
+    dropImage.addEventListener('dragover', (e) => {
+        e.preventDefault();
+    });
+    dropImage.addEventListener('drop', (e) => {
+        e.preventDefault();
+        let data = e.dataTransfer.getData("text");
+        e.target.appendChild(document.querySelector('#' + data));
+    });
+});
+window.addEventListener('DOMContentLoaded', () => {
+    let dropImage = document.querySelector('#dropImage8');
+    let dragImage = document.querySelector('#dragImage');
+    dragImage.addEventListener('dragstart', (e) => {
+        e.dataTransfer.setData("text", e.target.id);
+    });
+    dropImage.addEventListener('dragover', (e) => {
+        e.preventDefault();
+    });
+    dropImage.addEventListener('drop', (e) => {
+        e.preventDefault();
+        let data = e.dataTransfer.getData("text");
+        e.target.appendChild(document.querySelector('#' + data));
+    });
+});
+window.addEventListener('DOMContentLoaded', () => {
+    let dropImage = document.querySelector('#dropImage9');
+    let dragImage = document.querySelector('#dragImage');
+    dragImage.addEventListener('dragstart', (e) => {
+        e.dataTransfer.setData("text", e.target.id);
+    });
+    dropImage.addEventListener('dragover', (e) => {
+        e.preventDefault();
+    });
+    dropImage.addEventListener('drop', (e) => {
+        e.preventDefault();
+        let data = e.dataTransfer.getData("text");
+        e.target.appendChild(document.querySelector('#' + data));
+    });
+});
+window.addEventListener('DOMContentLoaded', () => {
+    let dropImage = document.querySelector('#dropImage10');
+    let dragImage = document.querySelector('#dragImage');
+    dragImage.addEventListener('dragstart', (e) => {
+        e.dataTransfer.setData("text", e.target.id);
+    });
+    dropImage.addEventListener('dragover', (e) => {
+        e.preventDefault();
+    });
+    dropImage.addEventListener('drop', (e) => {
+        e.preventDefault();
+        let data = e.dataTransfer.getData("text");
+        e.target.appendChild(document.querySelector('#' + data));
+    });
+});
