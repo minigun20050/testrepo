e3cca6b5a86bbf20ffd53882a0d1d0c82dd187ed
Fl5r <144325105+minigun20050@users.noreply.github.com>
2025-02-05 01:10:57 +0100

    Add files via upload

--- ProjRepo-main/script.js
+++ ProjRepo-main/script.js
@@ -1,8 +1,14 @@
-window.addEventListener("DOMContentLoaded", () => {
-    let btn = document.querySelectorAll("button[id='settings']");
-    btn.forEach((el) => {
-        el.addEventListener('click', () => {
-            document.getElementsByClassName("set").style.display='block';
-        })
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('themeToggle');
+  
+    button.addEventListener('click', function() {
+      if (document.body.classList.contains('dark')) {
+        document.body.classList.remove('dark');
+        document.body.classList.add('light');
+      } else {
+        document.body.classList.remove('light');
+        document.body.classList.add('dark');
+      }
     });
-});
+  });
+  
